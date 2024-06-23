c85eb506433017084696b92ec4e9c1d6303570db
FARUK2059 <omarfaruk34552@gmail.com>
2024-06-23 13:55:44 +0600

    update

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,14 @@
 const nextConfig = {
 
     images: {
-        domains: ['cdn.rareblocks.xyz'],
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "*",
+                port: "",
+                pathname: "**",
+            },
+        ],
     },
 
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,4 +17,24 @@ module.exports = {
   plugins: [
     require('daisyui'),
   ],
+  daisyui: {
+    themes: [
+      {
+        carDoctorTheme: {
+          ...require("daisyui/src/theming/themes")["light"],
+          primary: "#FF3811",
+          secondary: "teal",
+
+          ".btn-primary": {
+            "color" : "#fff"
+          },
+
+          ".btn-outline.btn-primary:hover": {
+            "color" : "#fff"
+          },
+        },
+      },
+      "dark",
+    ],
+  },
 };
