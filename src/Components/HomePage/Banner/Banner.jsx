c49b3077b35dee0d791355e22d064758f3f787c2
FARUK2059 <omarfaruk34552@gmail.com>
2024-10-20 04:12:12 +0600

    add download button

--- src/Components/HomePage/Banner/Banner.jsx
+++ src/Components/HomePage/Banner/Banner.jsx
@@ -11,6 +11,7 @@ import { Typewriter } from "react-simple-typewriter";
 import { TextRevealCard } from "@/components/ui/text-reveal-card";
 import { BackgroundLines } from "@/components/ui/background-lines";
 import LinkIcon from "@/components/Sheard/LinkIcon";
+import HoverButton from "@/components/Sheard/HoverButton";
 
 
 
@@ -79,94 +80,28 @@ const Banner = () => {
                             {/* <p className="max-w-lg mt-3 text-xl leading-relaxed  md:mt-8">I am developing a full-featured website using the MERN stack (MongoDB, Express.js, React, Node.js)
                                 to modernize and enhance user experience with a friendly, efficient, and contemporary design.</p> */}
 
-                            <p className="mt-4 text-xl text-green-400  md:mt-8">
+                            <p className="mt-1 text-xl text-green-400  md:mt-8">
                                 <span className="relative inline-block">
                                     <span className="absolute inline-block w-full bottom-0.5 h-1 bg-red-700"></span>
                                     <span className="relative"> Have a question? </span>
                                 </span>
-                                <br className="block sm:hidden" /> Ask me through the <Link href="#" title="" className="transition-all duration-200 text-slate-200 hover:text-slate-50 hover:underline">Contact</Link>
+                                <br className="block sm:hidden" /> Ask me through the <Link href="#contact" title="" className="transition-all duration-200 text-slate-200 hover:text-slate-50 hover:underline">Contact</Link>
                             </p>
 
                             {/* Link Section */}
-                            <div className="p-10">
+                            <div className="mt-10">
                                 <LinkIcon></LinkIcon>
                             </div>
 
-                            <div className="mt-6 flex justify-center gap-6 ">
-
-                                {/* Facebook */}
-                                <Link
-                                    className=" transition hover:text-yellow-200"
-                                    href="https://web.facebook.com/developerfarukk"
-                                    target="_blank"
-                                    rel="noreferrer"
-                                >
-                                    <span className="sr-only"> Facebook </span>
-                                    <SiFacebook className="h-6 w-6" />
-                                </Link>
-
-                                {/* Instagram */}
-                                <Link
-                                    className=" transition hover:text-yellow-200"
-                                    href="https://www.instagram.com/promoter_hridoy?igsh=ODdsbGJoZTJ0dmRj"
-                                    target="_blank"
-                                    rel="noreferrer"
-                                >
-                                    <span className="sr-only"> Instagram </span>
-                                    <FaInstagramSquare className="h-6 w-6" />
-                                </Link>
-
-                                {/* Twiter/X */}
-                                <Link
-                                    className=" transition hover:text-yellow-200"
-                                    href="https://x.com/Farukk2059?s=09"
-                                    target="_blank"
-                                    rel="noreferrer"
-                                >
-                                    <span className="sr-only"> Twiter/X </span>
-                                    <BsTwitterX className="h-6 w-6" />
-                                </Link>
-
-                                {/* GitHub */}
-                                <Link
-                                    className=" transition hover:text-yellow-200"
-                                    href="https://github.com/FARUK2059"
-                                    target="_blank"
-                                    rel="noreferrer"
-                                >
-                                    <span className="sr-only"> GitHub </span>
-                                    <FaGithub className="h-6 w-6" />
-                                </Link>
-
-                                {/* Linkedin */}
-                                <Link
-                                    className=" transition hover:text-yellow-200"
-                                    href="https://www.linkedin.com/in/omar-faruk7920?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BX7zN4PjASC6UkPE%2FI0Oslg%3D%3D"
-                                    target="_blank"
-                                    rel="noreferrer"
-                                >
-                                    <span className="sr-only"> LinkedIn </span>
-                                    <FaLinkedin className="h-6 w-6" />
-                                </Link>
-
-                                {/* Discord */}
-                                <Link
-                                    className=" transition hover:text-yellow-200"
-                                    href="https://www.discordapp.com/users/1247190729042956341"
-                                    target="_blank"
-                                    rel="noreferrer"
-                                >
-                                    <span className="sr-only"> Discord </span>
-                                    <FaDiscord className="h-6 w-6" />
-                                </Link>
-
-                            </div>
-
                             <div>
+                                <Link href="#" className="">
+                                    <HoverButton></HoverButton>
+                                </Link>
                                 <Link href="https://drive.google.com/file/d/1f712sf-H0d3PENlWP0m9u_I6SzdE0UhL/view?usp=sharing"
                                     download="M_Resume.pdf"
                                     target="blank"
                                     className="btn btn-primary bg-none p-2 m-6">Download Resume</Link>
+
                                 <Link href="/" className="btn btn-primary bg-none p-2 m-6">Hire Me</Link>
                             </div>
 
