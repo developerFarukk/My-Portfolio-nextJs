a4cc2f4f65493ad0e0fcb2913424055eba52defa
FARUK2059 <omarfaruk34552@gmail.com>
2024-10-20 22:21:18 +0600

    complet banner section

--- src/Components/HomePage/Banner/Banner.jsx
+++ src/Components/HomePage/Banner/Banner.jsx
@@ -13,6 +13,7 @@ import HireButton from "@/components/Sheard/HireButton";
 
 // Web Iamge
 import wave from "../../../../public/wave.gif"
+import BannerLamp from "./BannerLamp";
 
 // import Lottie from "lottie-react";
 // import { useLottie } from "lottie-react";
@@ -41,13 +42,14 @@ const Banner = () => {
 
     return (
         <div>
-            <section className="lg:mt-0 pt-20 overflow-hidden text-white  md:pt-16 sm:pt-20 2xl:pt-16">
-                <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
-                    <div className="grid container items-center text-center justify-between grid-cols-1 md:grid-cols-2 ">
+            <section className="lg:mt-0 pt-20 lg:pt-0 overflow-hidden text-white  md:pt-16 sm:pt-20 ">
+                <div className="">
+                    <div className="grid  items-center text-center justify-center grid-cols-1 md:grid-cols-2 lg:grid-cols-2 ">
 
+                        {/* Left side section */}
                         <div className="">
                             <h2 className=" text-3xl font-bold leading-tight  bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text">
-                                <div className="flex items-center justify-center gap-2">
+                                <div className="flex items-center justify-center gap-2 p-1">
                                     <h2> Hey </h2>
                                     <h2> <Image src={wave} alt="wave" height="40" className="-rotate-12 "></Image></h2>
                                     <h2> I am </h2>
@@ -56,7 +58,7 @@ const Banner = () => {
                                 <br className="block sm:hidden" />
 
                                 {/* Name Section */}
-                                <div className="flex items-center justify-center">
+                                <div className="flex items-center justify-center p-1">
                                     <TextRevealCard
                                         text="OMAR FARUK"
                                         revealText="Developer FARUK"
@@ -129,11 +131,20 @@ const Banner = () => {
 
                         </div>
 
-                        <div className="relative  flex items-center justify-center text-center">
+                        {/* Right side section */}
+                        <div className="relative  flex items-center justify-center text-center p-2">
+
+                            {/* <div className="relative w-auto">
+                                <BannerLamp></BannerLamp>
+                            </div> */}
+
+                            <div className="absolute ">
+                            <Image className="  opacity-70   wrapperimg rounded-full lg:p-16 md:p-12 p-8 " width={400} height={150} src="https://cdn.rareblocks.xyz/collection/celebration/images/team/1/blob-shape.svg" alt="" />
+                            </div>
 
                             <div>
-                                <BackgroundLines className="lg:mb-20  ">
-                                    <Image className="relative  xl:max-w-lg  xl:mx-auto 2xl:origin-bottom 2xl:scale-110" src={bg1} alt="My Responsive Image"
+                                <BackgroundLines className=" ">
+                                    <Image className="relative  " src={bg1} alt="My Responsive Image"
                                         layout="responsive"
 
                                     // width={500}
