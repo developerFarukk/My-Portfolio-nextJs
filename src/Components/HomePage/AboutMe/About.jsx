34a9a6e03543701e0335c7e3766a4ff2d2677bd5
FARUK2059 <omarfaruk34552@gmail.com>
2024-10-24 15:26:56 +0600

    up jsd

--- src/Components/HomePage/AboutMe/About.jsx
+++ src/Components/HomePage/AboutMe/About.jsx
@@ -33,6 +33,8 @@ const About = () => {
                         </h2>
                     </div>
 
+                    {/* <p>amar</p> */}
+
                     <div className="grid grid-cols-1   gap-y-6 md:grid-cols-2 gap-x-10 items-center ">
 
                         {/* Image Section */}
