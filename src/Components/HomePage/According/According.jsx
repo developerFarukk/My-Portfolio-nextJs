b8de29dfc4366fe4769f3f884fe45d6d48f79c32
FARUK2059 <omarfaruk34552@gmail.com>
2024-10-26 04:19:27 +0600

    update according

--- src/Components/HomePage/According/According.jsx
+++ src/Components/HomePage/According/According.jsx
@@ -1,16 +1,16 @@
 'use client'
 
+import { Accordion, AccordionItem } from "@nextui-org/accordion";
 
-import { Accordion, AccordionItem } from "@nextui-org/react";
 
 
 const According = () => {
 
     const accordings = [
         {
-            title: 'According 1',
+            title: <div className=""> According 1</div>,
             id: 1,
-            ariaLable: 'According 1',
+            ariaLable: <div className="border-none"> According 1</div>,
             Answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
         },
         {
@@ -29,13 +29,22 @@ const According = () => {
 
     return (
         <div>
-            <Accordion variant="splitted">
+            <div>
+                <div className="text-start">
+                    <span className="relative inline-block text-xl font-bold  text-orange-700 ">
+                        <span className="absolute inline-block w-full h-[1px] bg-white bottom-1 "></span>
+                        <span className="relative jersey-20-charted-regular "> Qustion</span>
+                    </span>
+                    <span className="font-bold text-xl text-slate-100"> : </span>
+                </div>
+            </div>
+            <Accordion variant="splitted" className="border-none  rounded-xl">
 
                 {
                     accordings.map((accor) => (
-                        
-                        <AccordionItem key={accor.id} aria-label={accor.ariaLable} title={accor.title} className="text-blue-100 bg-sky-950">
-                            {accor.Answer}
+
+                        <AccordionItem key={accor.id} aria-label={accor.ariaLable} title={accor.title} className="text-sky-300 font-semibold text-lg ">
+                            <span className="text-sky-600 text-start text-md"> {accor.Answer}</span>
                         </AccordionItem>
                     ))
                 }
