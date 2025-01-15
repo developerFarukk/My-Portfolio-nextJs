9b5f6feb472e6f35223e5c25f4666b7e76fa88bb
FARUK2059 <omarfaruk34552@gmail.com>
2025-01-16 05:16:57 +0600

    up pro

--- src/Components/HomePage/Projects/ProjectCard/ProjectsCard.jsx
+++ src/Components/HomePage/Projects/ProjectCard/ProjectsCard.jsx
@@ -11,7 +11,7 @@ const ProjectsCard = () => {
     return (
         <div>
             <CardContainer className="inter-var group">
-                <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[24rem] h-auto rounded-xl p-4 border shadow-[0px_0px_30px_1px_rgba(204,0,255,0.3)]  group-hover:shadow-[0px_0px_30px_1px_rgba(60,88,255,0.8)] hover:border-yellow-800 transition-all duration-3000">
+                <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[24rem] h-auto rounded-xl p-6 border shadow-[0px_0px_30px_1px_rgba(204,0,255,0.3)]  group-hover:shadow-[0px_0px_30px_1px_rgba(60,88,255,0.8)] hover:border-yellow-800 transition-all duration-3000">
                     <CardItem
                         translateZ="50"
                         className="text-xl font-bold text-neutral-600 dark:text-white"
