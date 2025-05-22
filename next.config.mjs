ef7bcfb6d3840f31ada17bbdb0bc701f8a54fb2e
FARUK2059 <omarfaruk34552@gmail.com>
2025-05-22 16:23:40 +0600

    fix update err

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,13 @@ const nextConfig = {
         ],
         unoptimized: true,
     },
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            '@': new URL('./src', import.meta.url).pathname,
+        };
+        return config;
+    }
 
 };
 
